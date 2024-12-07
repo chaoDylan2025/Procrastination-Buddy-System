822b7da43355ca2dfde96eff254357790ba48ff6
chaoDylan2025 <chaod@mail.gvsu.edu>
2024-12-06 23:51:51 -0500

    started work on focus list

--- procrastination-buddy/src/views/AboutView.vue
+++ procrastination-buddy/src/views/AboutView.vue
@@ -1,15 +1,24 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
-</template>
+  <v-app>
+    <div class="mt-10">
+      <v-container>
+        <v-row>
+          <v-col>
+            <p class="text-h4">What is Procrastination Buddy</p>
+            <p class="text-subtitle-1">It is a web application whose goal is to get its users to focus on their work by giving them the necessary tools that will help motivate and boost their productivity.</p>
+          </v-col>
+        </v-row>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
+        <v-row>
+          <v-col>
+            <p class="text-h4">Why use Procrastination Buddy</p>
+            <p class="text-subtitle-1">With Procrastination Buddy, you will have a personal page, you can upload any motivational images that can help you focus on your work. 
+             You will also be given a list of tasks that you can keep track of, and a focus list which is helpful for listing the sites that you do not want to visit.
+            </p>
+          </v-col>
+        </v-row>
+      </v-container>
+    </div>
+
+  </v-app>
+</template>
--- procrastination-buddy/src/views/FocusList.vue
+++ procrastination-buddy/src/views/FocusList.vue
@@ -1,22 +1,188 @@
-<script lang="js">
+<script setup>
+    import { userStore } from '../stores/user'
+    import { ref, onMounted } from 'vue'
+
+    const user = userStore()
+    
+    const dialog = ref(false)
+    const dialog2 = ref(false)
+    const dialog3 = ref(false)
+    const new_list_dialog = ref(false)
+    
+
+    var arr_of_tasks = ref([])
+    var new_restricted_wesbite = ref("")
+
+    //Test Array
+    var test_arr = ref(["https://www.youtube.com", "https://www.crunchyroll.com", "https://www.reddit.com/?rdt=33094"])
+
+    function dialog_confirm(){
+        dialog.value = false
+    }
+    function new_list_dialog_confirm(website){
+        test_arr.push(website)
+        new_list_dialog.value = false
+    }
+
+    function delete_website(index){
+        test_arr.value.splice(index, 1)
+        console.log(test_arr)
+    }
 </script>
 
 <template>
     <v-app>
-        <div class="mt-10" id="buttons-for-focus-list">
-            <v-btn> Create List </v-btn>
-            <v-btn> Edit List </v-btn>
-        </div>
+        <v-container>
+            <v-row class="mt-10">
+                <v-col></v-col>
+                <!-- Contains buttons that will each show a different dialog window -->
+                <v-col cols="10">
+                    <!-- Buttons are only shown if user is logged in -->
+                    <div v-if="user.isLoggedIn == false">
+                        <v-btn class="mr-4" @click="dialog = true"> Edit List </v-btn>
+                        <v-btn class="mr-4" @click="dialog2 = true"> Log Number of Sites Visited </v-btn>
+                    </div>
 
+                    <div v-else>
+                        <p class="text-h5"> Please login to your account </p>
+                    </div>
+                </v-col>
+                <v-col></v-col>
+            </v-row>
+        </v-container>
+
+        <!-- Dialog for when user clicks on 'Edit List' -->
+        <v-dialog v-model="dialog"
+            width="500">
+            <v-card
+            width="500"
+            title="Edit your current list of websites"
+            >
+                <!-- Contains current list of websites that a user can edit and delete -->
+                <v-container>
+                    <v-row>
+                        <v-list>
+                        <v-list-subheader>Restricted Websites</v-list-subheader>
+                            <v-list-item
+                                v-for="(item, i) in test_arr"
+                                :key="i"
+                                :value="item"
+                                color="primary"
+                                variant="plain"
+                            >
+                                <template v-slot:prepend>
+                                    <v-btn @click="delete_website(i)" density="compact" icon="mdi-delete"></v-btn>
+                                </template>
+                                <v-list-item-title v-text="item"></v-list-item-title>
+                            </v-list-item>
+                        </v-list>
+                    </v-row>
+                    
+                    <v-row>
+                        <v-col></v-col>
+                        <v-btn @click="new_list_dialog = true" density="compact" icon="mdi-plus"></v-btn>
+                        <v-col></v-col>
+                    </v-row>
+                </v-container>
+
+                <!-- Gives users the options to exit dialog or submit modified data -->
+                <v-card-actions>
+                    <v-btn
+                    text="Back"
+                    @click="dialog = false">
+                    </v-btn> 
+
+                    <v-btn
+                    text="Confirm"
+                    @click="dialog_confirm">
+                    </v-btn> 
+                </v-card-actions>
+                
+            </v-card>
+        </v-dialog>
+
+
+        <!-- Dialog for user to enter a new site -->
+        <v-dialog v-model="new_list_dialog"
+            width="500">
+            <v-card
+            width="500"
+            title="Enter a new site that you do not want to access"
+            >
+                <v-container>
+                    <v-text-field
+                    v-model="new_restricted_wesbite"
+                    hide-details="auto"
+                    label="Website"
+                    clearable
+                    ></v-text-field>
+                </v-container>
+
+                <v-card-actions>
+                    <v-btn
+                    text="Back"
+                    @click="new_list_dialog = false">
+                    </v-btn> 
+
+                    <v-btn
+                    text="Confirm"
+                    @click="new_list_dialog_confirm(new_restricted_wesbite)">
+                    </v-btn> 
+                </v-card-actions>
+            </v-card>
+        </v-dialog>
+
+
+        <!-- Dialog for when user clicks on 'Log Number Of Sites Visited' --> 
+        <v-dialog v-model="dialog2"
+            width="500">
+            <v-card
+            width="500"
+            title="Log number of times you visited each website"
+            >
+                <v-container>
+                    <v-row>
+                        <v-list>
+                        <v-list-subheader>Restricted Websites</v-list-subheader>
+                            <v-list-item
+                                v-for="(item, i) in test_arr"
+                                :key="i"
+                                :value="item"
+                                color="primary"
+                                variant="plain"
+                            >
+                                <template v-slot:prepend>
+                                    <v-text-field>
+                                        <template v-slot:prepend>
+                                            <v-icon>
+                                                mdi-minus
+                                            </v-icon>
+                                        </template>
+
+                                        <template v-slot:append>
+                                            <v-icon>
+                                                mdi-plus
+                                            </v-icon>
+                                        </template>
+                                    </v-text-field>
+                                </template>
+                                <v-list-item-title v-text="item" class="ml-2"></v-list-item-title>
+                            </v-list-item>
+                        </v-list>
+                    </v-row>
+                </v-container>
+                <v-card-actions>
+                    <v-btn
+                    text="Back"
+                    @click="dialog2 = false">
+                    </v-btn> 
+
+                    <v-btn
+                    text="Confirm"
+                    @click="dialog2 = false">
+                    </v-btn> 
+                </v-card-actions>
+            </v-card>
+        </v-dialog>
     </v-app>
 </template>
-
-<style lang="css" scoped>
-    #buttons-for-focus-list{
-        display: flex;
-        justify-content: center;
-    }
-    #buttons-for-focus-list > button {
-        margin-right: 45px;
-    }
-</style>
