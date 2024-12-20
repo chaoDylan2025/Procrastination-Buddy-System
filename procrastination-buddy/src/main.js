da808417b742d6976530ac3ce379c2ac986c05b3
chaoDylan2025 <chaod@mail.gvsu.edu>
2024-12-19 20:57:36 -0500

    started setting up firebase

--- procrastination-buddy/src/main.js
+++ procrastination-buddy/src/main.js
@@ -13,6 +13,26 @@ import * as directives from "vuetify/directives";
 
 import App from './App.vue'
 import router from './router'
+import dotenv from 'dotenv'
+dotenv.config()
+
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore"
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
+};
+
+// Initialize Firebase
+const firebase = initializeApp(firebaseConfig);
+const analytics = getAnalytics(firebase);
 
 const vuetify = createVuetify({
     icons: {
