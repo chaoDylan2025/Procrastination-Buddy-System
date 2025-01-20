1e27891295e694a2509becc1d6558d966356ac27
chaoDylan2025 <chaod@mail.gvsu.edu>
2025-01-20 17:26:04 -0500

    started working on profile dialog modals

---
+++ procrastination-buddy/src/components/ChangePassword.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { ref } from 'vue'
+const props = defineProps({
+    open_change_password_dialog: Boolean
+})
+
+const emit = defineEmits('close')
+
+function exitDialogModal(){
+    emit('close', false)
+}
+</script>
+
+<template>
+    <v-app>
+        <v-dialog
+            v-model="props.open_change_password_dialog"
+            width="500"
+        >
+            <v-card>
+                <v-row>
+                    <v-spacer></v-spacer>
+                    <v-card-actions>
+
+                    </v-card-actions>
+                        <v-btn @click="exitDialogModal(false)">Back</v-btn>
+                        <v-btn>Confirm</v-btn> 
+                </v-row>
+            </v-card>
+        </v-dialog>
+    </v-app>
+</template>
--- procrastination-buddy/src/views/Profile.vue
+++ procrastination-buddy/src/views/Profile.vue
@@ -2,6 +2,16 @@
     import { RouterLink, RouterView } from 'vue-router'
     import { ref, onMounted } from 'vue'
     import AuthenticationService from '../services/AuthenticationService'
+
+    // Importing components
+    import ChangeEmail from '../components/ChangeEmail.vue'
+    import ChangePassword from '../components/ChangePassword.vue'
+    import DeleteAccount from '../components/DeleteAccount.vue'
+
+    var open_email_change_dialog = ref(false)
+    var open_change_password_dialog = ref(false)
+    var open_delete_account_dialog = ref(false)
+
 </script>
 
 <template>
@@ -9,7 +19,7 @@
         <v-row class="mt-10">
             <v-col></v-col>
             <v-col>
-                <v-btn class="ml-16"> Change Email Address </v-btn>
+                <v-btn class="ml-16" @click="open_email_change_dialog = true"> Change Email Address </v-btn>
             </v-col>
             <v-col></v-col>
         </v-row>
@@ -17,7 +27,7 @@
         <v-row class="mt-10">
             <v-col></v-col>
             <v-col>
-                <v-btn class="ml-16"> Change Password </v-btn>
+                <v-btn class="ml-16" @click="open_change_password_dialog = true"> Change Password </v-btn>
             </v-col>
             <v-col></v-col>
         </v-row>
@@ -25,7 +35,7 @@
         <v-row class="mt-10">
             <v-col></v-col>
             <v-col>
-                <v-btn class="ml-16"> Delete Account </v-btn>
+                <v-btn class="ml-16" @click="open_delete_account_dialog = true"> Delete Account </v-btn>
             </v-col>
             <v-col></v-col>
         </v-row>
@@ -39,5 +49,8 @@
         </v-row>
     </v-container>
 
+    <ChangeEmail :open_change_email_dialog="open_email_change_dialog" @close="(state) => open_email_change_dialog = state" />
+    <ChangePassword :open_change_password_dialog="open_change_password_dialog" @close="(state) => open_change_password_dialog = state" />
+    <DeleteAccount :open_delete_account_dialog="open_delete_account_dialog" @close="(state) => open_delete_account_dialog = state"/>
 
 </template>
