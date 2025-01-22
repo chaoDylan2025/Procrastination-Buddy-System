10b93923124a9eb0da7fc97a4929c302c61e2c03
chaoDylan2025 <chaod@mail.gvsu.edu>
2025-01-22 14:45:39 -0500

    made simple UIs for deleting account and changing password

--- procrastination-buddy/src/components/ChangePassword.vue
+++ procrastination-buddy/src/components/ChangePassword.vue
@@ -15,17 +15,42 @@ function exitDialogModal(){
     <v-app>
         <v-dialog
             v-model="props.open_change_password_dialog"
+            height="500"
             width="500"
         >
             <v-card>
-                <v-row>
-                    <v-spacer></v-spacer>
-                    <v-card-actions>
-
-                    </v-card-actions>
-                        <v-btn @click="exitDialogModal(false)">Back</v-btn>
-                        <v-btn>Confirm</v-btn> 
+                <v-card-title class="text-h5 font-weight-black"> Change your current password </v-card-title>
+                <v-row class="mt-5 ml-5 mr-5">
+                    <v-responsive
+                            class="mx-auto"
+                            m-height="345"
+                    >
+                        <v-text-field label="Current password"></v-text-field>
+                    </v-responsive>
                 </v-row>
+
+                <v-row class="mt-5 ml-5 mr-5">
+                    <v-responsive
+                            class="mx-auto"
+                            m-height="345"
+                    >
+                        <v-text-field label="Enter new password"></v-text-field>
+                    </v-responsive>
+                </v-row>
+
+                <v-row class="mt-5 ml-5 mr-5">
+                    <v-responsive
+                            class="mx-auto"
+                            m-height="345"
+                    >
+                        <v-text-field label="Re-enter new password"></v-text-field>
+                    </v-responsive>
+                </v-row>
+
+                <v-card-actions>
+                    <v-btn @click="exitDialogModal(false)">Back</v-btn>
+                    <v-btn>Confirm</v-btn>
+                </v-card-actions> 
             </v-card>
         </v-dialog>
     </v-app>
--- procrastination-buddy/src/components/DeleteAccount.vue
+++ procrastination-buddy/src/components/DeleteAccount.vue
@@ -15,17 +15,25 @@ function exitDialogModal(){
     <v-app>
         <v-dialog
             v-model="props.open_delete_account_dialog"
+            height="200"
             width="500"
         >
             <v-card>
-                <v-row>
-                    <v-spacer></v-spacer>
-                    <v-card-actions>
+                <v-card-title class="text-h6 font-weight-black"> Are you sure you want to delete your account? </v-card-title>
+                <v-row class="mt-5">
+                    <v-responsive
+                            class="ml-10 mr-10"
+                            height="auto"
+                    >
+                        <v-text-field label="Current password"></v-text-field>
+                    </v-responsive>
+                    
 
-                    </v-card-actions>
-                        <v-btn @click="exitDialogModal(false)">Back</v-btn>
-                        <v-btn>Confirm</v-btn> 
                 </v-row>
+                <v-card-actions>
+                    <v-btn @click="exitDialogModal(false)">No</v-btn>
+                    <v-btn>Yes</v-btn>
+                </v-card-actions>
             </v-card>
         </v-dialog>
     </v-app>
