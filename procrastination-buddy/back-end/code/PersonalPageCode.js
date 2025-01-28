92dadf9193ec9355d744d9755607170d26ef727f
chaoDylan2025 <chaod@mail.gvsu.edu>
2025-01-28 10:18:41 -0500

    configured appwrite

--- procrastination-buddy/back-end/code/PersonalPageCode.js
+++ procrastination-buddy/back-end/code/PersonalPageCode.js
@@ -1,27 +1,13 @@
-// Path to Cloud Storage bucket
-const bucketName = 'user_motivational_content'
+import dotenv from 'dotenv'
+dotenv.config()
 
-// Imports the Google Cloud client library
-import { Storage } from '@google-cloud/storage'
+import {Client, Account} from 'appwrite'
 
-// Creates a client
-const storage = new Storage()
+export const client = new Client()
 
-// Array of image files
-var current_images_arr = []
+client
+  .setEndpoint('https://cloud.appwrite.io/v1')
+  .setProject(process.env.APPWRITE_PROJECTID)
 
-// Retrieves images of current user
-export async function getImages() {
-  // Lists files in the bucket
-  const [files] = await storage.bucket(bucketName).getFiles()
-
-  // Fill up array if it is empty
-  if(current_images_arr.length == 0){
-    // Iterates through each image of current user
-    files.forEach(file => {
-      let file_path = `https://storage.googleapis.com/${bucketName}/${file.name}`
-      current_images_arr.push(file_path)
-    });
-  }
-  return current_images_arr
-}
+export const account = new Account(client)
+export { ID } from 'appwrite'
