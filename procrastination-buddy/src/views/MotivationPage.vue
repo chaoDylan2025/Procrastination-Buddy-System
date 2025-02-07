d5a7669e82006e04516437e0d2365e76946f2919
chaoDylan2025 <chaod@mail.gvsu.edu>
2025-02-07 16:51:58 -0500

    made changes to buttons in personal motivational page UI

--- procrastination-buddy/src/views/MotivationPage.vue
+++ procrastination-buddy/src/views/MotivationPage.vue
@@ -2,10 +2,6 @@
     import AuthenticationService from '../services/AuthenticationService'
     import {ref} from 'vue'
     import Content from './Content.vue'
-    import StorageView from '../components/StorageView.vue';
-
-    // Opens Storage dialog modal
-    var open_storage_dialog_modal = ref(false)
 
 </script>
 
@@ -16,11 +12,11 @@
              <v-row class="ml-10">
                 <v-spacer></v-spacer>
                 <v-col class="mt-5">
-                    <v-btn @click="">Edit Content</v-btn>
+                    <v-btn @click="" size="small">Edit Content</v-btn>
                 </v-col>
                 <v-spacer></v-spacer>
                 <v-col class="mt-5">
-                    <v-btn @click="open_storage_dialog_modal = true">Storage</v-btn>
+                    <v-btn @click="open_storage_dialog_modal = true" size="small">Download Images</v-btn>
                 </v-col>
                 <v-spacer></v-spacer>
              </v-row>
@@ -31,10 +27,5 @@
              </v-row>
              <v-row></v-row>
         </v-container>
-
-        <StorageView :open_storage_view="open_storage_dialog_modal" @close="(state) => open_storage_dialog_modal = state"/>
-
-
     </v-app>
-
 </template>
