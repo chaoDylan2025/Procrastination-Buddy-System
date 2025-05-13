f59f265afdf17413d287df2bcd19eca6c9dbe9a7
chaoDylan2025 <chaod@mail.gvsu.edu>
2025-05-13 18:11:41 -0400

    moved firebase setup to a different file

--- procrastination-buddy/back-end/code/user.js
+++ procrastination-buddy/back-end/code/user.js
@@ -1,28 +1,6 @@
-import dotenv from 'dotenv'
-dotenv.config()
-
-import { initializeApp } from "firebase/app"
 import * as firebaseAuth from "firebase/auth"
-import { addDoc, collection, doc, getFirestore, getDocs, setDoc } from "firebase/firestore"
-
-const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID
-}
-
-// Initialize Firebase
-const firebase = initializeApp(firebaseConfig);
-
-// Initialize Firestore
-const db = getFirestore(firebase)
-
-// Accesses authentication code
-const auth = firebaseAuth.getAuth()
+import { addDoc, collection, doc, getDocs, setDoc } from "firebase/firestore"
+import { db, auth } from "../firebase_setup.js"
 
 // Contains signed-in user's email
 var user_email = null
