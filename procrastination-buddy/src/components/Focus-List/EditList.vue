3a7bdf058245a9e6fc6b8cde553b9754fa758248
chaoDylan2025 <chaod@mail.gvsu.edu>
2025-07-03 14:45:19 -0400

    refactored edit list dialog into different files

---
+++ procrastination-buddy/src/components/Focus-List/EditList.vue
@@ -0,0 +1,70 @@
+<script setup>
+import InsertWebsite from './InsertWebsite.vue'
+import { ref } from 'vue'
+
+const props = defineProps({
+    open_edit_list_dialog: Boolean,
+    current_web_list: Array
+})
+
+const emit = defineEmits(['close'])
+
+var open_new_list_dialog = ref(false)
+
+// Deletes website in the list
+function delete_website(index){
+    props.current_web_list.splice(index, 1)
+}
+
+// Inserts new website in current restricted websites list
+function insertNewWebsite(state, website){
+    if(website != ""){
+        props.current_web_list.push(website);
+    }
+    open_new_list_dialog.value = state
+}
+
+function exitDialogModal(){
+    emit('close', false)
+}
+</script>
+
+<template>
+    <v-app>
+        <!-- Dialog for when user clicks on 'Edit List' button -->
+        <v-dialog v-model="props.open_edit_list_dialog" width="500">
+            <v-card
+            width="500"
+            title="Edit your current list of websites"
+            >
+                <div class="font-weight-bold text-center mb-3" style="color: black;"> Restricted Websites </div>
+                
+                <!-- Contains current list of websites that a user can edit and delete -->
+                <div class="d-flex ml-4 mb-4" v-for="(item, i) in props.current_web_list">
+                    <div>
+                        <v-btn class="mr-5" @click="delete_website(i)" density="compact" icon="mdi-delete"></v-btn>
+                    </div>
+                    <div> {{ item }} </div>
+                </div>
+
+                <!-- Users can press this button to add another restricted website to their list -->
+                <v-row>
+                    <v-col></v-col>
+                    <v-btn @click="open_new_list_dialog = true" density="compact" icon="mdi-plus"></v-btn>
+                    <v-col></v-col>
+                </v-row>
+
+                <!-- Gives users the option to exit the dialog -->
+                <v-card-actions>
+                    <v-btn
+                    text="Back"
+                    @click="exitDialogModal()">
+                    </v-btn> 
+                </v-card-actions>
+            </v-card>
+        </v-dialog>
+
+        <!-- Dialog for inserting a new restricted website into the list -->
+        <InsertWebsite :open_make_new_list_dialog="open_new_list_dialog" :current_web_list="props.current_web_list" @close="insertNewWebsite"/>
+    </v-app>
+</template>
--- procrastination-buddy/src/views/FocusList.vue
+++ procrastination-buddy/src/views/FocusList.vue
@@ -1,38 +1,18 @@
 <script setup>
-    import AuthenticationService from '../services/AuthenticationService';  
-    import { userStore } from '../stores/user'
-    import { ref, onMounted } from 'vue'
+import AuthenticationService from '../services/AuthenticationService';  
+import EditList from '../components/Focus-List/EditList.vue';
+import { userStore } from '../stores/user'
+import { ref, onMounted } from 'vue'
 
-    // Pinia store for user information
-    const user = userStore()
-    
-    // Reactive variables
-    const dialog = ref(false)
-    const dialog2 = ref(false)
-    const dialog3 = ref(false)
-    const new_list_dialog = ref(false)
-    
+// Pinia store for user information
+const user = userStore()
 
-    var arr_of_tasks = ref([])
-    var new_restricted_wesbite = ref("")
+// Reactive variables
+const edit_list_dialog = ref(false)
+const dialog2 = ref(false)
 
-    //Test Array
-    var test_arr = ref(["https://www.youtube.com", "https://www.crunchyroll.com", "https://www.reddit.com/?rdt=33094"])
-
-    function dialog_confirm(){
-        dialog.value = false
-    }
-    // Inserts new website in the list
-    function new_list_dialog_confirm(website){
-        test_arr.value.push(website)
-        new_list_dialog.value = false
-    }
-
-    // Deletes website in the list
-    function delete_website(index){
-        test_arr.value.splice(index, 1)
-        console.log(test_arr)
-    }
+//Test Array
+var test_arr = ref(["https://www.youtube.com", "https://www.crunchyroll.com", "https://www.reddit.com/?rdt=33094"])
 </script>
 
 <template>
@@ -45,7 +25,7 @@
                     <!-- Buttons are only shown if user is logged in -->
                     <div class="d-flex flex-column">
                         <v-btn class="rounded-pill" size="small" @click="dialog2 = true"> Log Sites Visited </v-btn>
-                        <v-btn class="rounded-pill mt-4" size="small" @click="dialog = true"> Edit List </v-btn>
+                        <v-btn class="rounded-pill mt-4" size="small" @click="edit_list_dialog = true"> Edit List </v-btn>
                     </div>
 
                     <div>
@@ -57,7 +37,7 @@
                                     </h3>
                                 </span>
                             </div>
-                            
+
                             <v-list-item
                                 v-for="(item, i) in test_arr"
                                 :key="i"
@@ -79,83 +59,9 @@
             <h2 class="text-center"> Please login to your account </h2>
         </v-container>
 
-        <!-- Dialog for when user clicks on 'Edit List' -->
-        <v-dialog v-model="dialog"
-            width="500">
-            <v-card
-            width="500"
-            title="Edit your current list of websites"
-            >
-                <!-- Contains current list of websites that a user can edit and delete -->
-                <v-container>
-                    <v-row>
-                        <v-list>
-                            <v-list-subheader>Restricted Websites</v-list-subheader>
-                            <v-list-item
-                                v-for="(item, i) in test_arr"
-                                :key="i"
-                                :value="item"
-                                style="text-align: center;"
-                                color="primary"
-                                variant="plain"
-                            >
-                                <template v-slot:prepend>
-                                    <v-btn @click="delete_website(i)" density="compact" icon="mdi-delete"></v-btn>
-                                </template>
-                                <v-list-item-title v-text="item"></v-list-item-title>
-                            </v-list-item>
-                        </v-list>
-                    </v-row>
-                    
-                    <v-row>
-                        <v-col></v-col>
-                        <v-btn @click="new_list_dialog = true" density="compact" icon="mdi-plus"></v-btn>
-                        <v-col></v-col>
-                    </v-row>
-                </v-container>
-
-                <!-- Gives users the option to exit dialog -->
-                <v-card-actions>
-                    <v-btn
-                    text="Back"
-                    @click="dialog = false">
-                    </v-btn> 
-                </v-card-actions>
-            </v-card>
-        </v-dialog>
-
-
-        <!-- Dialog for user to enter a new site -->
-        <v-dialog v-model="new_list_dialog"
-            width="500">
-            <v-card
-            width="500"
-            title="Enter a new site that you do not want to access"
-            >
-                <v-container>
-                    <!-- Asks user to enter new restricted website -->
-                    <v-text-field
-                    v-model="new_restricted_wesbite"
-                    hide-details="auto"
-                    label="Website"
-                    clearable
-                    ></v-text-field>
-                </v-container>
-
-                <v-card-actions>
-                    <v-btn
-                    text="Back"
-                    @click="new_list_dialog = false">
-                    </v-btn> 
-
-                    <v-btn
-                    text="Confirm"
-                    @click="new_list_dialog_confirm(new_restricted_wesbite)">
-                    </v-btn> 
-                </v-card-actions>
-            </v-card>
-        </v-dialog>
-
+        <v-container>
+            <EditList :open_edit_list_dialog="edit_list_dialog" :current_web_list="test_arr" @close="(state) => edit_list_dialog=state"/>
+        </v-container>
 
         <!-- Dialog for when user clicks on 'Log Number Of Sites Visited' --> 
         <v-dialog v-model="dialog2"
@@ -198,12 +104,12 @@
                 <v-card-actions>
                     <v-btn
                     text="Back"
-                    @click="dialog2 = false">
+                    @click="">
                     </v-btn> 
 
                     <v-btn
                     text="Confirm"
-                    @click="dialog2 = false">
+                    @click="">
                     </v-btn> 
                 </v-card-actions>
             </v-card>
