6974210fa082cf6a08be2f0cd7dcddbfa64a83f6
chaoDylan2025 <chaod@mail.gvsu.edu>
2025-07-30 17:47:43 -0400

    created a dialog for changing email

--- procrastination-buddy/src/components/Profile/ChangeEmail.vue
+++ procrastination-buddy/src/components/Profile/ChangeEmail.vue
@@ -1,8 +1,11 @@
 <script setup>
 import { ref } from 'vue'
+
 const props = defineProps({
     open_change_email_dialog: Boolean
 })
+
+var new_email_address = ref("")
 
 const emit = defineEmits('close')
 
@@ -16,16 +19,23 @@ function exitDialogModal(){
         <v-dialog
             v-model="props.open_change_email_dialog"
             width="500"
+            height="250"
         >
             <v-card>
-                <v-row>
-                    <v-spacer></v-spacer>
-                    <v-card-actions>
-
-                    </v-card-actions>
-                        <v-btn @click="exitDialogModal(false)">Back</v-btn>
-                        <v-btn>Confirm</v-btn> 
+                <v-card-title class="text-h5 text-center font-weight-black"> Enter a new email address to change to </v-card-title>
+                <v-row class="ma-8">
+                    <v-responsive
+                            class="mx-auto"
+                            m-height="345"
+                    >
+                        <v-text-field v-model="new_email_address" 
+                        label="New Email Address"></v-text-field>
+                    </v-responsive>
                 </v-row>
+                <v-card-actions>
+                    <v-btn @click="exitDialogModal(false)">Back</v-btn>
+                    <v-btn>Confirm</v-btn> 
+                </v-card-actions>
             </v-card>
         </v-dialog>
     </v-app>
