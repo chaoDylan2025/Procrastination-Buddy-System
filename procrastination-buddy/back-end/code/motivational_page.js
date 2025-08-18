b0ef2e9b6cb629dbd61137d1e432a5b3e9a1452d
chaoDylan2025 <chaod@mail.gvsu.edu>
2025-08-17 22:47:25 -0400

    added detailed comments to motivational page backend code

--- procrastination-buddy/back-end/code/motivational_page.js
+++ procrastination-buddy/back-end/code/motivational_page.js
@@ -2,9 +2,13 @@ import { doc, getDoc, updateDoc } from "firebase/firestore"
 import { db } from "../firebase_setup.js"
 import { user_email } from "./user.js"
 
+// Reference and snapshot for current document
 var docRef = ""
 var docSnap = ""
 
+/**
+ * Get the current user's motivational images and preferred image layout
+ */
 export async function getImagesAndLayout(){
     if (user_email != null){
         docRef = doc(db, "users", user_email, "personal-motivational-page", "current_images")
@@ -13,6 +17,11 @@ export async function getImagesAndLayout(){
     }
 }
 
+/**
+ * Provides default motivational images for new user
+ * 
+ * @param images - Default motivational images 
+ */
 export async function setDefaultImages(images){
     if (user_email != null){
         docRef = doc(db, "users", user_email, "personal-motivational-page", "current_images")
@@ -24,6 +33,12 @@ export async function setDefaultImages(images){
     return false
 }
 
+/**
+ * Update the current user's motivational images and preferred image layout
+ * 
+ * @param image_layout - Current user's new preferred image layout
+ * @param images - Current user's new motivational images
+ */
 export async function updateImagesAndLayout(image_layout, images){
     if (user_email != null){
         docRef = doc(db, "users", user_email, "personal-motivational-page", "current_images")
