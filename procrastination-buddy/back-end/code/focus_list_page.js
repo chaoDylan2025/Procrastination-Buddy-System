ab07cc7c374f854f384bfd2a526dd49bb806397c
chaoDylan2025 <chaod@mail.gvsu.edu>
2025-08-17 22:52:33 -0400

    added detailed comments to focus list backend code

--- procrastination-buddy/back-end/code/focus_list_page.js
+++ procrastination-buddy/back-end/code/focus_list_page.js
@@ -2,9 +2,13 @@ import { doc, getDoc, updateDoc } from "firebase/firestore"
 import { db } from "../firebase_setup.js"
 import { user_email } from "./user.js"
 
+// Reference and snapshot for current document
 var docRef = ""
 var docSnap = ""
 
+/**
+ * Get the current user's restricted sites list 
+ */
 export async function getRestrictedSitesList(){
     if (user_email != null){
         docRef = doc(db, "users", user_email, "focus-list", "restricted_sites")
@@ -13,10 +17,15 @@ export async function getRestrictedSitesList(){
     }
 }
 
-export async function setRestrictedSiteList(website){
+/**
+ * Update the current user's restricted sites list
+ * 
+ * @param site_list - New restricted sites list
+ */
+export async function setRestrictedSiteList(site_list){
     if (user_email != null){
         docRef = doc(db, "users", user_email, "focus-list", "restricted_sites")
-        docSnap = await updateDoc(docRef, website)
+        docSnap = await updateDoc(docRef, site_list)
         return true
     }
     return false
