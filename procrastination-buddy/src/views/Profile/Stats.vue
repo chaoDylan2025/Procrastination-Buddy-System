0f2b9efe8943fb25c6d53d7ac7d377f8ac9a90ac
chaoDylan2025 <chaod@mail.gvsu.edu>
2025-08-30 18:43:51 -0400

    mostly made css improvements to stats page

--- procrastination-buddy/src/views/Profile/Stats.vue
+++ procrastination-buddy/src/views/Profile/Stats.vue
@@ -1,8 +1,22 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import { ref, onMounted } from 'vue'
 import AuthenticationService from '../../services/AuthenticationService'
+import { ref, onMounted } from 'vue'
 
+var list_of_sites = ref([]) // Actual array from database
+
+// Results to display for each stat
+var number_sites_result = ref("Please add a website")
+var most_visited_result = ref("Please add a website")
+var least_visited_result = ref("Please add a website")
+
+// Gets the current user's restricted websites list
+onMounted(async () => {
+    let result = await AuthenticationService.restrictedWebsitesList();
+    list_of_sites.value = result.data.list
+    console.log("List of sites: ", list_of_sites.value)
+    console.log("Number of restricted websites: ", list_of_sites.value.length)
+    console.log("Most visited restricted website: ")
+})
 </script>
 
 <template>
@@ -10,7 +24,7 @@ import AuthenticationService from '../../services/AuthenticationService'
         <v-row>
             <v-col></v-col>
             <v-col>
-                <p class="text-center text-h4"> Focus List Stats </p>
+                <p class="mt-3 text-center text-h4"> Focus List Stats </p>
             </v-col>
             <v-col></v-col>
         </v-row>
@@ -19,34 +33,34 @@ import AuthenticationService from '../../services/AuthenticationService'
             <div>
                 <div class="mb-5 d-flex flex-column text-center">
                     <div>
-                        
+                        <p class="mt-3 font-weight-bold"> Number of restricted websites </p>
                     </div>
 
                     <div>
-                        <p class="mt-2"> Number of restricted websites </p>
+                        <p class="mt-2"> {{ number_sites_result }} </p>
                     </div>
                 </div>
 
                 <div class="mb-5 d-flex flex-column text-center">
                     <div>
+                        <p class="mt-3 font-weight-bold"> Most visited restricted website </p>
                     </div>
-                    
+
                     <div>
-                        <p class="mt-2"> Most visited restricted website </p>
+                        <p class="mt-2"> {{ most_visited_result }} </p>
                     </div>
                 </div>
 
                 <div class="d-flex flex-column text-center">
                     <div>
+                        <p class="mt-3 font-weight-bold"> Least visited restricted website </p>
                     </div>
-                    
+
                     <div>
-                        <p class="mt-2"> Least visited restricted website </p>
+                        <p class="mt-2"> {{ least_visited_result }} </p>
                     </div>
                 </div>
             </div>
-
-            
         </v-row>
     </v-container>
 </template>
