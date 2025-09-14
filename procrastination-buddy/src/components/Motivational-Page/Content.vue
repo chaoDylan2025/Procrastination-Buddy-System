5710d6b807715346b7024682ea372490fd6486a8
chaoDylan2025 <chaod@mail.gvsu.edu>
2025-09-14 17:56:55 -0400

    implemented functionality that prevents duplicate selected images

--- procrastination-buddy/src/components/Motivational-Page/Content.vue
+++ procrastination-buddy/src/components/Motivational-Page/Content.vue
@@ -16,12 +16,24 @@ const user = userStore()
 
 var open_change_image_dialog = ref(false) 
 var openImage = ref(false)
+
+/**
+ * Checks if current motivational image has been selected
+ * 
+ * @param index - Current motivational image index
+ */
+function hasMotivationalImg(index){
+    const result = current_imgs.value.some((img) => {
+        return img.image === motivational_imgs.value[index].image
+    })
+    return result
+}
 </script>
 
 <template>
     <v-app class="pa-6">
         <!-- Current selected images -->
-        <v-container class="mt-10">
+        <v-container>
             <v-row>
                 <v-col v-if="!show_all_images" v-for="(img, i) in current_imgs"
                         :key="i"
@@ -48,20 +60,25 @@ var openImage = ref(false)
                         :value="img"
                         cols="12"
                 >
-                    <v-row class="justify-center mb-12">
-                        <img :src=img.image height="200px" :class="img.styling" @click="displayConfirmButtonEvnt(i, img)"></img>    
-                    </v-row>
-                    <v-row class="mt-5 mb-8">
-                        <v-spacer></v-spacer>
-                        <v-btn size="x-small" @click="openImage = viewButtonEvnt(img.image, openImage)">
-                            View
-                        </v-btn>
-                        <v-spacer></v-spacer>
+                    <v-row class="d-flex flex-column" v-if="hasMotivationalImg(i) == false">
+                        <v-row class="mt-4 justify-center">
+                            <img :src=motivational_imgs[i].image height="200px" :class="img.styling" @click="displayConfirmButtonEvnt(i, img)"></img>
+                        </v-row>
+                        
+                        <v-row class="mt-6 mb-8">
+                            <v-spacer></v-spacer>
+                            <v-btn size="x-small" @click="openImage = viewButtonEvnt(img.image, openImage)">
+                                View
+                            </v-btn>
+                            <v-spacer></v-spacer>
+                        </v-row>    
                     </v-row>
                 </v-col>
                 
-                <ChangeImage :open_change_image_dialog="open_change_image_dialog" @close="(state) => open_change_image_dialog = closeImageChangingDialog(state)"/>
-                <Image :image="imageToBeViewed" :open_image="openImage" @close="(state) => openImage = state"/>
+                <v-container v-show="false">
+                    <ChangeImage :open_change_image_dialog="open_change_image_dialog" @close="(state) => open_change_image_dialog = closeImageChangingDialog(state)"/>
+                    <Image :image="imageToBeViewed" :open_image="openImage" @close="(state) => openImage = state"/>
+                </v-container>
             </v-row>
         </v-container>
     </v-app>
