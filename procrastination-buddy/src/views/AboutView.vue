e4b66fe4ed002a8b6c6d45be50f0e7fafe3d4a12
chaoDylan2025 <chaod@mail.gvsu.edu>
2025-09-14 21:11:38 -0400

    made slight change to about section

--- procrastination-buddy/src/views/AboutView.vue
+++ procrastination-buddy/src/views/AboutView.vue
@@ -12,7 +12,7 @@
         <v-row>
           <v-col>
             <p class="text-h4">Why use Procrastination Buddy</p>
-            <p class="text-subtitle-1">With Procrastination Buddy, you will have a motivation page, a list of tasks that you can keep track of, and a focus list which is helpful for listing the sites that you do not want to visit.
+            <p class="text-subtitle-1">With Procrastination Buddy, you will have a customizable motivation page and a focus list which is helpful for listing the websites that you do not want to visit.
             </p>
           </v-col>
         </v-row>
